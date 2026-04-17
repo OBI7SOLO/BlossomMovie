cc30aa21fdf03e3afb52c3c320a151418d18b8cc
OBI7SOLO <andersonlosada.11@gmail.com>
2026-04-17 00:07:59 -0500

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Anderson Losada on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
