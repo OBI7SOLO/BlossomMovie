5d98d5556e73cfeb7bc3980d83f736f0b5c2cf03
OBI7SOLO <andersonlosada.11@gmail.com>
2026-04-17 02:18:00 -0500

    Section 1

---
+++ BlossomMovie/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  BlossomMovie
+//
+//  Created by Anderson Losada on 17/04/26.
+//
+
+import Foundation
---
+++ BlossomMovie/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BlossomMovie
+//
+//  Created by Anderson Losada on 17/04/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
